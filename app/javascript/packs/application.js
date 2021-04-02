6587fff810896b6b933c29fae481ae74ec285ad3
yoshi-1023 <yoshika.122355@gmail.com>
2021-04-02 11:32:25 +0900

    ルートパスへアクセスでitems/index.html.erbが表示される

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,7 +4,7 @@
 // that code so it'll be compiled.
 
 require("@rails/ujs").start()
-require("turbolinks").start()
+// require("turbolinks").start() ←削除する（画面遷移を高速化するGem）
 require("@rails/activestorage").start()
 require("channels")
 
