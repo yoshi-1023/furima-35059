6eb8a535a8b5538cd7841c83bfebaa6e4d1f4c83
yoshi-1023 <yoshika.122355@gmail.com>
2021-04-06 14:30:34 +0900

    バリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,15 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
-  validates :last_name, presence: true
-  validates :first_name, presence: true
-  validates :last_name_kana, presence: true
-  validates :first_name_kana, presence: true
-  validates :birth_day, presence: true       
+  validates :last_name, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/, message: "Full-width characters"}
+  validates :first_name, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/, message: "Full-width characters"}
+  validates :last_name_kana, presence: true, format: {with:/\A[ァ-ヶー－]+\z/, message: "Full-width katakana characters"}
+  validates :first_name_kana, presence: true, format: {with:/\A[ァ-ヶー－]+\z/, message: "Full-width katakana characters"}
+  validates :birth_day, presence: true
+
+  validates :password, presence: true, format: {with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,}+\z/i, message: "Include both letters and numbers"}
+
+
+
+  
 end
