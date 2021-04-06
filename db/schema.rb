1bc5b8a6efb6b3d7d32bc917dd9071b79d9545d4
yoshi-1023 <yoshika.122355@gmail.com>
2021-04-06 18:43:07 +0900

    商品モデルと商品テーブルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_05_053543) do
+ActiveRecord::Schema.define(version: 2021_04_06_090639) do
+
+  create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "item_name", null: false
+    t.text "item_text", null: false
+    t.integer "category_id", null: false
+    t.integer "product_status_id", null: false
+    t.integer "shipping_charges_id", null: false
+    t.integer "prefectures_id", null: false
+    t.integer "shipping_date_id", null: false
+    t.integer "price", null: false
+    t.bigint "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_items_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", null: false
@@ -30,4 +45,5 @@ ActiveRecord::Schema.define(version: 2021_04_05_053543) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "items", "users"
 end
