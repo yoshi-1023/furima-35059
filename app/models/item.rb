c1153bac8a2dfc95c07999a69129844205d3caff
yoshi-1023 <yoshika.122355@gmail.com>
2021-04-08 13:07:11 +0900

    商品出品機能のバリデーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,16 +6,18 @@ class Item < ApplicationRecord
  
 
   with_options presence: true do
+    validates :image
     validates :item_name
     validates :item_text
-    with_options numericality: { other_than: 1 } do
+    with_options numericality: { other_than: 1 ,message: "Select"} do
       validates :category_id
       validates :product_status_id
       validates :shipping_charges_id
       validates :prefectures_id
       validates :shipping_date_id
     end
-    validates :price, format: { with: /\A[0-9]+\z/, message: "Half-width number" }, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "Out of setting range" }
+    validates :price
   end
-
+  validates :price, numericality: { only_integer: true, message: "Half-width number" }
+  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "Out of setting range" }
 end
