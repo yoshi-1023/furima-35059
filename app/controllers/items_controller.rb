be8096f44fbcdb029ab0cb87c263dd0083ff540a
yoshi-1023 <yoshika.122355@gmail.com>
2021-04-08 15:21:51 +0900

    商品出品機能のログイン状態の制限の変更

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, expect: [:index]
+  before_action :authenticate_user!, only: [:new]
   def index
     @items = Item.all
   end
