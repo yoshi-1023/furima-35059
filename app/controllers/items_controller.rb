f1ec017faba2f9a423e9b7ff2e001d426d7a7477
yoshi-1023 <yoshika.122355@gmail.com>
2021-04-09 15:56:39 +0900

    ログイン状態の出品者のみ、出品した商品の商品情報編集ページに遷移

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new]
+  before_action :authenticate_user!, only: [:new, :edit]
   def index
     @items = Item.all.order(created_at: :desc)
   end
@@ -19,6 +19,16 @@ class ItemsController < ApplicationController
 
   def show
     @item = Item.find(params[:id])
+  end
+
+  def edit
+    @item = Item.find(params[:id])
+    unless @item.user_id == current_user.id
+      redirect_to action: :index
+    end
+  end
+
+  def update
   end
 
   private
