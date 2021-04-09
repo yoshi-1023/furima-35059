2e80bccd84816b6dda20845ddc3276773bda49c2
yoshi-1023 <yoshika.122355@gmail.com>
2021-04-09 20:11:52 +0900

    Revert "商品削除機能の実装"
    This reverts commit 04c0664aa55e23020c12bd62681900c252ca15da.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :edit, :detroy]
-  before_action :set_item, only: [:show, :edit, :update, :destroy]
-  before_action :move_to_index, only: [:edit, :update, :destroy]
+  before_action :authenticate_user!, only: [:new, :edit]
+  before_action :set_item, only: [:show, :edit, :update]
+  before_action :move_to_index, only: [:edit, :update]
 
   def index
     @items = Item.all.order(created_at: :desc)
@@ -32,12 +32,6 @@ class ItemsController < ApplicationController
     else
      render :edit
     end 
-  end
-
-  def destroy
-    if @item.destroy
-      redirect_to root_path
-    end
   end
 
   private
