6eafaf65315812afe6d7542bc1700f0aea68f84a
yoshi-1023 <yoshika.122355@gmail.com>
2021-04-13 13:03:09 +0900

    商品購入ページのエラーメッセージの変更

--- app/models/order_shipping.rb
+++ app/models/order_shipping.rb
@@ -7,7 +7,7 @@ class OrderShipping
     validates :user_id
     validates :item_id
     validates :post_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'Input correctly' }
-    validates :prefectures_id, numericality: { other_than: 1, message: "can't be blank" }
+    validates :prefectures_id, numericality: { other_than: 1, message: "Select" }
     validates :city
     validates :address
     validates :phone_number, format: { with: /\A[0-9]{11}\z/, message: 'Input only number' }
