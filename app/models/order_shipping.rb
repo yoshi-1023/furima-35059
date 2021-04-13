3b3d94999a9aa67a30af5204a3d027652c37a9f5
yoshi-1023 <yoshika.122355@gmail.com>
2021-04-13 17:13:25 +0900

    app/models/order_shipping.rbの電話番号の正規表現の変更

--- app/models/order_shipping.rb
+++ app/models/order_shipping.rb
@@ -10,7 +10,7 @@ class OrderShipping
     validates :prefectures_id, numericality: { other_than: 1, message: "Select" }
     validates :city
     validates :address
-    validates :phone_number, format: { with: /\A[0-9]{11}\z/, message: 'Input only number' }
+    validates :phone_number, format: { with: /\A[0-9]{10,11}\z/, message: 'Input only number' }
   end
   
 
